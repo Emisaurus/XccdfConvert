58bd627d4e7ba7aa41cdc21f6e2a82d8c31d8b7c
Emisaurus <54713806+Emisaurus@users.noreply.github.com>
2021-12-20 10:15:34 -0600

    Add files via upload

--- xccdfconvert/xccdfConvert/xccdfconvert.java
+++ xccdfconvert/xccdfConvert/xccdfconvert.java
@@ -6,10 +6,10 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.StringReader;
 import java.lang.String;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.UUID;
@@ -49,11 +49,11 @@ public class xccdfconvert {
 	public static void main(String[] args) {
 		long timestart, timeend, duration;
 		SortedMap<String, String> mapFiles = loadStigs();
+		List<File> filelist;
 
 		switch(args[0])
 		{
 			case "-d":
-				List<File> filelist;
 				//ArrayList<File> filelist = new ArrayList<>();
 				File top_directory = new File(args[1]);
 				if(top_directory.exists()  && top_directory.isDirectory()) {
@@ -71,7 +71,8 @@ public class xccdfconvert {
 			case "-o":
 				File xccdffile = new File(args[1]);
 				timestart = System.nanoTime();
-				File[] filelist = {xccdffile};
+				filelist = new ArrayList<File>();
+				filelist.add(xccdffile);
 				convert(filelist, null);
 				timeend = System.nanoTime();
 				duration = timeend - timestart;
@@ -182,181 +183,94 @@ public class xccdfconvert {
 	//import xccdf results
 	//merge the two
 	//output checklist
-	public static void convert(File xccdffile, File checklistfile)
+	public static void convert(List<File> results, File checklistfile)
 	{
+		SortedMap<String, String> mapFiles = loadStigs();
+		SortedMap<String, CHECKLIST> checklists = new TreeMap<>();
 		JAXBContext jaxbContext;
-		
-		CHECKLIST checklist = new CHECKLIST();
-		
-		//settings
-		//regex for hostname and xccdf
-		//stiglibrary?
+		CHECKLIST checklist;
 		
 		try
 		{	
-			//for testing
-			SortedMap<String, String> mapFiles = new TreeMap<String, String>();
-			mapFiles.put("Red Hat Enterprise Linux 7 Security Technical Implementation Guide",
-					"C:\\Users\\Emisaurus\\Downloads\\U_SRG-STIG_Library_2021_10v2\\"
-					+ "U_RHEL_7_V3R5_Manual_STIG\\U_RHEL_7_STIG_V3R5_Manual-xccdf.xml");
-			
-			//Change XML to Java Object
-		    jaxbContext = JAXBContext.newInstance(SCAPBench.class);
-		    Unmarshaller jaxbUnmarshallerXccdf = jaxbContext.createUnmarshaller();
-		    SCAPBench benchmark = (SCAPBench) jaxbUnmarshallerXccdf.unmarshal(xccdffile);
-			    
-			//Import blank checklist
-		    File stigFile = new File(mapFiles.get(benchmark.getTitle()));
-		    jaxbContext = JAXBContext.newInstance(STIGBench.class);
-		    Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-		    STIGBench stig = (STIGBench) jaxbUnmarshaller.unmarshal(stigFile);
-			
-		    //Merge the xccdf files
-		    stig.setTestResult(benchmark.getTestResult());
-		    
-		    //CHECKLIST - ASSET
-		    ASSET asset = checklist.getASSET();
-		    //use java uuid library
-		    UUID uuid = UUID.randomUUID();
-		    UUID stiguuid = UUID.randomUUID();
-
-		    //Converting Xccdf Elements to Checklist
-		    String hostip = null, hostmac = null, hostfqdn = null;
-		    for(FactType value : stig.getTestResult().getTargetFacts().getFact())
-		    {
-		    	if(value.getName().equals("urn:scap:fact:asset:identifier:ipv4") 
-		    			&& !value.getValue().equals("127.0.0.1"))
-		    		hostip = value.getValue();
-		    	if(value.getName().equals("urn:scap:fact:asset:identifier:mac") 
-		    			&& !value.getValue().equals("00:00:00:00:00:00"))
-		    		hostmac = value.getValue();
-		    	if(value.getName().equals("urn:scap:fact:asset:identifier:fqdn"))
-					hostfqdn = value.getValue();
-		    }
-		    asset.setHOSTNAME(stig.getTestResult().getTarget());
-		    asset.setROLE("None");
-		    asset.setASSETTYPE("Computing");
-		    asset.setHOSTIP(hostip);
-		    asset.setHOSTMAC(hostmac);
-		    asset.setHOSTFQDN(hostfqdn);
-		    asset.setTARGETCOMMENT("");
-		    asset.setTECHAREA("");
-		    asset.setTARGETKEY(stig.getGroup().get(0).getRule().getReference().getIdentifier());
-		    asset.setWEBDBSITE("");
-		    asset.setWEBDBINSTANCE("");
-		    
-		    //CHECKLIST - ISTIG
-		    List<ISTIG> istiglist = checklist.getSTIGS().getISTIG();
-		    ISTIG istig = new ISTIG();
-		    
-		    //CHECKLIST - ISTIG - STIGINFO
-		    List<SIDATA> stiginfo = istig.getSTIGINFO().getSIDATA();
-		    //Version
-		    //Assumes Plaintext item 0 is the release-info
-		    stiginfo.add(new SIDATA("version", stig.getVersion()));
-		    //Classification - unused
-		    stiginfo.add(new SIDATA("classification", "UNCLASSIFIED"));
-		    //customname - unused
-		    stiginfo.add(new SIDATA("customname", null));
-		    //stigid
-		    stiginfo.add(new SIDATA("stigid", stig.getId()));
-		    //description
-		    stiginfo.add(new SIDATA("description", stig.getDescription()));
-		    //filename
-		    stiginfo.add(new SIDATA("filename", stigFile.getName()));
-		    //releaseinfo
-		    stiginfo.add(new SIDATA("releaseinfo", stig.getPlainText().get(0).getValue()));
-		    //title
-		    stiginfo.add(new SIDATA("title", stig.getTitle()));
-		    //uuid
-		    stiginfo.add(new SIDATA("uuid", uuid.toString()));
-		    //notice
-		    stiginfo.add(new SIDATA("notice", stig.getNotice().getId()));
-		    //source - unused
-		    stiginfo.add(new SIDATA("source", null));
-		    
-		    //CHECKLIST - ISTIG - VULNS
-		    List<GroupType> groups = stig.getGroup();
-		    List<VULN> vulns = istig.getVULN();
-		    TestResultType testresult = stig.getTestResult();
-			for(GroupType group : groups)
+			for(File xccdffile : results)
 			{
-				VULN vuln = new VULN();
-				List<STIGDATA> stigdata = vuln.getSTIGDATA();
-				RuleType rule = group.getRule();
-				stigdata.add(new STIGDATA("Vuln_Num", group.getId()));
-				stigdata.add(new STIGDATA("Severity", rule.getSeverity().value()));
-				stigdata.add(new STIGDATA("Group_Title", group.getTitle()));
-				stigdata.add(new STIGDATA("Rule_ID", rule.getId()));
-				stigdata.add(new STIGDATA("Rule_Ver", rule.getVersion()));
-				stigdata.add(new STIGDATA("Rule_Title", rule.getTitle()));
-				
-				StringReader sr = new StringReader("<VulnData>" + rule.getDescription() + "</VulnData>");
-				jaxbContext = JAXBContext.newInstance(VulnData.class);
-				Unmarshaller unmarshallerVuln = jaxbContext.createUnmarshaller();
-				VulnData vulndata = (VulnData) unmarshallerVuln.unmarshal(sr);
-				
-				stigdata.add(new STIGDATA("Vuln_Discuss", vulndata.getVulnDiscuss()));
-				stigdata.add(new STIGDATA("IA_Controls", vulndata.getIAControls()));
-				stigdata.add(new STIGDATA("Check_Content", rule.getCheck().getCheckContent()));
-				stigdata.add(new STIGDATA("Fix_Text", rule.getFixtext().getValue()));
-				stigdata.add(new STIGDATA("False_Positives", vulndata.getFalsePositive()));
-				stigdata.add(new STIGDATA("False_Negatives", vulndata.getFalseNegative()));
-				stigdata.add(new STIGDATA("Documentable", String.valueOf(vulndata.getDocumentable())));
-				stigdata.add(new STIGDATA("Mitigations", vulndata.getMitigations()));
-				stigdata.add(new STIGDATA("Potential_Impact", vulndata.getImpacts()));
-				stigdata.add(new STIGDATA("Third_Party_Tools", vulndata.getThirdParty()));
-				stigdata.add(new STIGDATA("Mitigation_Control", vulndata.getMitigations()));
-				stigdata.add(new STIGDATA("Responsibility", vulndata.getResponsibility()));
-				stigdata.add(new STIGDATA("Security_Override_Guidance", vulndata.getSeverityOverride()));
-				stigdata.add(new STIGDATA("Check_Content_Ref", rule.getCheck().getCheckContentRef().getName()));
-				stigdata.add(new STIGDATA("Weight", String.valueOf(rule.getWeight())));
-				stigdata.add(new STIGDATA("Class", "Unclass"));
-				stigdata.add(new STIGDATA("STIGRef", 
-						stig.getTitle() + " :: Version " + 
-						stig.getVersion() + ", " + 
-						stig.getPlainText().get(0).getValue()));
-				stigdata.add(new STIGDATA("TargetKey", rule.getReference().getIdentifier()));
-				//We have no stig uuid
-				stigdata.add(new STIGDATA("STIG_UUID", stiguuid.toString()));
-				for(IdentType ident : rule.getIdent())
-				{
-					if(ident.getSystem().equals("http://cyber.mil/legacy"))
-						stigdata.add(new STIGDATA("LEGACY_ID", ident.getValue()));
-					if(ident.getSystem().equals("http://cyber.mil/cci"))
-						stigdata.add(new STIGDATA("CCI_REF", ident.getValue()));
-				}
-				
-				//Setting default
-				vuln.setSTATUS("Not_Reviewed");
-				vuln.setFINDINGDETAILS("");
-				
-				for(RuleResultType ruleresult : testresult.getRuleResult())
-				{
-					if(ruleresult.getVersion().equals(rule.getVersion()))
-					{
-						vuln.setSTATUS(ruleresult.getResult().name());
-						vuln.setFINDINGDETAILS("Tool: " + testresult.getTestSystem()
-								+ "\nTime: " + ruleresult.getTime()
-								+ "\nResult: " + ruleresult.getResult().value());
-						testresult.getRuleResult().remove(ruleresult);
-						break;
-					}
+				//Change XML to Java Object
+			    jaxbContext = JAXBContext.newInstance(SCAPBench.class);
+			    Unmarshaller jaxbUnmarshallerXccdf = jaxbContext.createUnmarshaller();
+			    SCAPBench benchmark = (SCAPBench) jaxbUnmarshallerXccdf.unmarshal(xccdffile);
+			    
+			    if(mapFiles.containsKey(benchmark.getTitle()))
+			    {
+					//Import blank checklist
+				    File stigFile = new File(mapFiles.get(benchmark.getTitle()));
+				    jaxbContext = JAXBContext.newInstance(STIGBench.class);
+				    Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+				    STIGBench stig = (STIGBench) jaxbUnmarshaller.unmarshal(stigFile);
+				    
+				    //Merge the xccdf files
+				    stig.setTestResult(benchmark.getTestResult());
+				    
+				    //Grab the Server name from filename
+				    String stigname = xccdffile.getName().split("_")[0];
+				    System.out.println(xccdffile.getName()); //testing
+				    
+				    if(!checklists.containsKey(stigname))
+				    {
+				    	checklist = new CHECKLIST();
+				    	
+					    //CHECKLIST - ASSET
+					    ASSET asset = checklist.getASSET();
+
+					    //Converting Xccdf Elements to Checklist
+					    String hostip = null, hostmac = null, hostfqdn = null;
+					    for(FactType value : stig.getTestResult().getTargetFacts().getFact())
+					    {
+					    	if(value.getName().equals("urn:scap:fact:asset:identifier:ipv4") 
+					    			&& !value.getValue().equals("127.0.0.1"))
+					    		hostip = value.getValue();
+					    	if(value.getName().equals("urn:scap:fact:asset:identifier:mac") 
+					    			&& !value.getValue().equals("00:00:00:00:00:00"))
+					    		hostmac = value.getValue();
+					    	if(value.getName().equals("urn:scap:fact:asset:identifier:fqdn"))
+								hostfqdn = value.getValue();
+					    }
+					    asset.setHOSTNAME(stig.getTestResult().getTarget());
+					    asset.setROLE("None");
+					    asset.setASSETTYPE("Computing");
+					    asset.setHOSTIP(hostip);
+					    asset.setHOSTMAC(hostmac);
+					    asset.setHOSTFQDN(hostfqdn);
+					    asset.setTARGETCOMMENT("");
+					    asset.setTECHAREA("");
+					    asset.setTARGETKEY(stig.getGroup().get(0).getRule().getReference().getIdentifier());
+					    asset.setWEBDBSITE("");
+					    asset.setWEBDBINSTANCE("");
+				    }else
+				    {
+				    	checklist = checklists.get(stigname);
+				    }	    
+				    
+				    //CHECKLIST - ISTIG
+				    List<ISTIG> istiglist = checklist.getSTIGS().getISTIG();
+					istiglist.add(convertISTIG(stigFile.getName(), stig, mapFiles));
+				    checklists.put(stigname, checklist);
 					
-				}
-				vuln.setCOMMENTS("");
-				vuln.setSEVERITYOVERRIDE("");
-				vuln.setSEVERITYJUSTIFICATION("");
-				vulns.add(vuln);
+			    }else
+			    {
+			    	System.out.println("Unable to load the STIG: " + benchmark.getTitle() + "from the library");
+			    	System.out.println("Skipping STIG. Please import manually.");
+			    }			    
 			}
-			istiglist.add(istig);
 			
-		    //Checklist Java to XML
-			checklistfile = new File(".\\checklist.ckl");
-			jaxbContext = JAXBContext.newInstance(CHECKLIST.class);    
-		    Marshaller jaxbmarshaller = jaxbContext.createMarshaller();
-		    jaxbmarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE );
-		    jaxbmarshaller.marshal(checklist, checklistfile);
+		    //Write out each completed compiled Checklist
+			for (Entry<String, CHECKLIST> entry : checklists.entrySet())
+			{
+				checklistfile = new File(".\\" + entry.getKey() + ".ckl");
+				jaxbContext = JAXBContext.newInstance(CHECKLIST.class);    
+			    Marshaller jaxbmarshaller = jaxbContext.createMarshaller();
+			    jaxbmarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE );
+			    jaxbmarshaller.marshal(entry.getValue(), checklistfile);
+		    }
 		     
 		    System.out.println("woooo?");
 		}
@@ -364,6 +278,104 @@ public class xccdfconvert {
 		{
 		    e.printStackTrace();
 		}
+	}
+	
+	private static ISTIG convertISTIG(String filename, STIGBench stig, SortedMap<String, String> mapFiles) throws JAXBException
+	{
+		JAXBContext jaxbContext;
+		ISTIG istig = new ISTIG();
+		//TODO:Check if uuid is random each time for istig
+	    UUID uuid = UUID.randomUUID();
+	    UUID stiguuid = UUID.randomUUID();
+	    
+	    //CHECKLIST - ISTIG - STIGINFO
+	    List<SIDATA> stiginfo = istig.getSTIGINFO().getSIDATA();
+	    stiginfo.add(new SIDATA("version", stig.getVersion()));
+	    stiginfo.add(new SIDATA("classification", "UNCLASSIFIED"));
+	    stiginfo.add(new SIDATA("customname", null));
+	    stiginfo.add(new SIDATA("stigid", stig.getId()));
+	    stiginfo.add(new SIDATA("description", stig.getDescription()));
+	    stiginfo.add(new SIDATA("filename", filename));
+	    stiginfo.add(new SIDATA("releaseinfo", stig.getPlainText().get(0).getValue()));
+	    stiginfo.add(new SIDATA("title", stig.getTitle()));
+	    stiginfo.add(new SIDATA("uuid", uuid.toString()));
+	    stiginfo.add(new SIDATA("notice", stig.getNotice().getId()));
+	    stiginfo.add(new SIDATA("source", null));
+	    
+	    //CHECKLIST - ISTIG - VULNS
+	    List<GroupType> groups = stig.getGroup();
+	    List<VULN> vulns = istig.getVULN();
+	    TestResultType testresult = stig.getTestResult();
+		for(GroupType group : groups)
+		{
+			VULN vuln = new VULN();
+			List<STIGDATA> stigdata = vuln.getSTIGDATA();
+			RuleType rule = group.getRule();
+			stigdata.add(new STIGDATA("Vuln_Num", group.getId()));
+			stigdata.add(new STIGDATA("Severity", rule.getSeverity().value()));
+			stigdata.add(new STIGDATA("Group_Title", group.getTitle()));
+			stigdata.add(new STIGDATA("Rule_ID", rule.getId()));
+			stigdata.add(new STIGDATA("Rule_Ver", rule.getVersion()));
+			stigdata.add(new STIGDATA("Rule_Title", rule.getTitle()));
+			
+			StringReader sr = new StringReader("<VulnData>" + rule.getDescription() + "</VulnData>");
+			jaxbContext = JAXBContext.newInstance(VulnData.class);
+			Unmarshaller unmarshallerVuln = jaxbContext.createUnmarshaller();
+			VulnData vulndata = (VulnData) unmarshallerVuln.unmarshal(sr);
+			
+			stigdata.add(new STIGDATA("Vuln_Discuss", vulndata.getVulnDiscuss()));
+			stigdata.add(new STIGDATA("IA_Controls", vulndata.getIAControls()));
+			stigdata.add(new STIGDATA("Check_Content", rule.getCheck().getCheckContent()));
+			stigdata.add(new STIGDATA("Fix_Text", rule.getFixtext().getValue()));
+			stigdata.add(new STIGDATA("False_Positives", vulndata.getFalsePositive()));
+			stigdata.add(new STIGDATA("False_Negatives", vulndata.getFalseNegative()));
+			stigdata.add(new STIGDATA("Documentable", String.valueOf(vulndata.getDocumentable())));
+			stigdata.add(new STIGDATA("Mitigations", vulndata.getMitigations()));
+			stigdata.add(new STIGDATA("Potential_Impact", vulndata.getImpacts()));
+			stigdata.add(new STIGDATA("Third_Party_Tools", vulndata.getThirdParty()));
+			stigdata.add(new STIGDATA("Mitigation_Control", vulndata.getMitigations()));
+			stigdata.add(new STIGDATA("Responsibility", vulndata.getResponsibility()));
+			stigdata.add(new STIGDATA("Security_Override_Guidance", vulndata.getSeverityOverride()));
+			stigdata.add(new STIGDATA("Check_Content_Ref", rule.getCheck().getCheckContentRef().getName()));
+			stigdata.add(new STIGDATA("Weight", String.valueOf(rule.getWeight())));
+			stigdata.add(new STIGDATA("Class", "Unclass"));
+			stigdata.add(new STIGDATA("STIGRef", 
+					stig.getTitle() + " :: Version " + 
+					stig.getVersion() + ", " + 
+					stig.getPlainText().get(0).getValue()));
+			stigdata.add(new STIGDATA("TargetKey", rule.getReference().getIdentifier()));
+			stigdata.add(new STIGDATA("STIG_UUID", stiguuid.toString()));
+			for(IdentType ident : rule.getIdent())
+			{
+				if(ident.getSystem().equals("http://cyber.mil/legacy"))
+					stigdata.add(new STIGDATA("LEGACY_ID", ident.getValue()));
+				if(ident.getSystem().equals("http://cyber.mil/cci"))
+					stigdata.add(new STIGDATA("CCI_REF", ident.getValue()));
+			}
+			
+			//Setting default
+			vuln.setSTATUS("Not_Reviewed");
+			vuln.setFINDINGDETAILS("");
+			
+			for(RuleResultType ruleresult : testresult.getRuleResult())
+			{
+				if(ruleresult.getVersion().equals(rule.getVersion()))
+				{
+					vuln.setSTATUS(ruleresult.getResult().name());
+					vuln.setFINDINGDETAILS("Tool: " + testresult.getTestSystem()
+							+ "\nTime: " + ruleresult.getTime()
+							+ "\nResult: " + ruleresult.getResult().value());
+					testresult.getRuleResult().remove(ruleresult);
+					break;
+				}
+				
+			}
+			vuln.setCOMMENTS("");
+			vuln.setSEVERITYOVERRIDE("");
+			vuln.setSEVERITYJUSTIFICATION("");
+			vulns.add(vuln);
+		}
+		return istig;
 	}
 	
 	public static void error(Exception e)
